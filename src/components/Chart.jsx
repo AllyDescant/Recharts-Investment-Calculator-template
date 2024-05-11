25c54ecf881bce7f89322ef9a42742b43e88e6e5
AllyDescant <128736090+AllyDescant@users.noreply.github.com>
2024-05-11 08:53:03 -0500

    Update Chart.jsx

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,80 +1,34 @@
-import React from 'react';
-import { isEmpty } from 'lodash';
-import { Box } from '@mui/material';
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import React from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid,
+} from "recharts";
 
-const convertToCurrency = (value) => {
-  const options = { style: 'currency', currency: 'USD' };
-  return value.toLocaleString('en-US', options);
-}
-
-const tooltipFormatter = (value, name) => {
-  const customNames = {
-    amount: 'Total amount',
-    totalContributions: "Total contributions",
-  }
-
-  return [convertToCurrency(value), customNames[name]]
-}
-
-const EmptyState = () => <Box>Fill in some values to get started</Box>;
-
-const Visualization = ({ yearlyReturns }) => {
-  const finalYear = yearlyReturns[yearlyReturns.length - 1];
-  
+function App() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <Box sx={{ fontWeight: "bold", fontSize: '20px', mb: '20px' }}>
-        {`You will have ${convertToCurrency(finalYear.amount)} by ${finalYear.year}`}
-      </Box>
-      <ResponsiveContainer width="90%" height="90%">
-        <LineChart
-          width={800}
-          height={600}
-          data={yearlyReturns}
-          margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="year" />
-          <YAxis />
-          <Tooltip formatter={tooltipFormatter} />
-          <Line type="monotone" dataKey="amount" stroke="#4841cc" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="totalContributions" stroke="#9c2624" activeDot={{ r: 8 }} />
-        </LineChart>
-      </ResponsiveContainer>
-    </Box>
-  )
-}
-
-const Chart = ({ yearlyReturns }) => {
-  return (
-    <Box sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '70%', 
-      backgroundColor: '#e4eaf5',
-      borderRadius: '0px 50px 50px 0px',
-    }}>
-      {isEmpty(yearlyReturns) ?
-        <EmptyState /> :
-        <Visualization yearlyReturns={yearlyReturns} />
-      }
-    </Box>
+    <ResponsiveContainer width="90%" height={400}>
+      <AreaChart data={data}>
+        <defs>
+          <linearGradient id="gradient" x1="1" y2="1">
+            <stop offset="10%" stopColor="#08009e" stopOpacity={1} />
+            <stop offset="100%" stopColor="#08009e" stopOpacity={0.1} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Area dataKey="score" type="monotone" fill="url(#gradient)" />
+        <Tooltip />
+        <Legend />
+        <CartesianGrid strokeDasharray="3 3" />
+      </AreaChart>
+    </ResponsiveContainer>
   );
 }
 
-export default Chart;
+export default App;
